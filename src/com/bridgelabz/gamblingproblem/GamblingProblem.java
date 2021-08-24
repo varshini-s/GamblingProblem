9c2d1059393d3c9b51dba2a1c81de1043eb05b0b
varshini <dreamvarshini@gmail.com>
2021-08-24 10:02:43 +0530

    ADDED:code to calculate total stake

--- src/com/bridgelabz/gamblingproblem/GamblingProblem.java
+++ src/com/bridgelabz/gamblingproblem/GamblingProblem.java
@@ -6,6 +6,8 @@ public class GamblingProblem
 	public static final int BET_EVERY_GAME=1;
 	
 	public static int stake=STAKE;
+	public static int amountWon=0,amountLost=0;
+	public static int totalStake = 0;
 	
 	public static boolean makeBet()
 	{
@@ -41,26 +43,45 @@ public class GamblingProblem
 		
 	}
 
+	public static void  countStake()
+	{
+		
+		totalStake=totalStake+stake;
+		
+		
+	}
+	
 	public static void main(String[] args) 
 	{
 		boolean resign=false;
 		boolean betResult=false;
 		System.out.println("Welcoem to gambling problem");
 		
-		while(resign==false)
+		int numberOfDays=20;
+		int countDays=0;
+		int amount=0;
+		
+		while(countDays<numberOfDays)
 		{
-			betResult=makeBet();
-			if(resignStake()==true)
+			stake=STAKE;
+			
+			while(resign==false)
 			{
-				System.out.println("Player resigned the game");
-				break;
+				betResult=makeBet();
+				if(resignStake()==true)
+				{
+					System.out.println("Player resigned the game");
+					countStake();
+					break;
+				}
 			}
+			
+			countDays++;
+			
 		}
 		
-		
-		
-		
-
+		System.out.println("After "+countDays+" days stake is "+totalStake);
+	
 	}
 
 }
