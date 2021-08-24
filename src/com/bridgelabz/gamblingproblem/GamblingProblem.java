b5f4d7a081ea5dd8c2872fb58ede6940e7885c2d
varshini <dreamvarshini@gmail.com>
2021-08-24 09:26:45 +0530

    ADDED:new method to resign game

--- src/com/bridgelabz/gamblingproblem/GamblingProblem.java
+++ src/com/bridgelabz/gamblingproblem/GamblingProblem.java
@@ -2,13 +2,16 @@ package com.bridgelabz.gamblingproblem;
 
 public class GamblingProblem 
 {
-	public static int STAKE=100;
-	public static int BET_EVERY_GAME=1;
+	public static final int STAKE=100;
+	public static final int BET_EVERY_GAME=1;
+	
+	public static int stake=STAKE;
 	
 	public static boolean makeBet()
 	{
 		boolean betWin=false;
 		int output=(int)Math.floor(Math.random()*10)%2;
+		
 		
 		if(output==1)
 		{
@@ -18,27 +21,42 @@ public class GamblingProblem
 		else if(output==0)
 		{
 			stake--;
-			System.out.println("Gambler ");
+			System.out.println("Gambler lost ");
 		}
 		return betWin;
+	}
+	public static boolean resignStake()
+	
+	{ boolean resign=false;
+		
+		int stakePerecentage=(stake*100)/STAKE;
+		
+		System.out.println(stake);
+		if(stakePerecentage>=150 || stakePerecentage<50)
+		{
+			
+			resign=true;
+		}
+		return resign;
+		
 	}
 
 	public static void main(String[] args) 
 	{
+		boolean resign=false;
 		boolean betResult=false;
 		System.out.println("Welcoem to gambling problem");
 		
+		while(resign==false)
+		{
+			betResult=makeBet();
+			if(resignStake()==true)
+			{
+				System.out.println("Player resigned the game");
+				break;
+			}
+		}
 		
-		betResult=makeBet();
-		if(betResult==true)
-		{
-			
-		}
-		else
-		{
-			
-
-		}
 		
 		
 		
