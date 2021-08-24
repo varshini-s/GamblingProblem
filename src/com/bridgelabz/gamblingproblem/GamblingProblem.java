730b39c8dfe67ce005b1932f7e28468dc6c99e1a
varshini <dreamvarshini@gmail.com>
2021-08-24 08:22:05 +0530

    ADDED:program to print welcome message

---
+++ src/com/bridgelabz/gamblingproblem/GamblingProblem.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.gamblingproblem;
+
+public class GamblingProblem 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Welcoem to gambling problem");
+		
+
+	}
+
+}
