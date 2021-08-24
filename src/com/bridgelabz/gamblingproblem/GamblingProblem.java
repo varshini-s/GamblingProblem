ea7c0e4c8d583e9720bcfd7c50b4a8bc08ac96bd
varshini <dreamvarshini@gmail.com>
2021-08-24 08:48:39 +0530

    ADDED:new method to make bet

--- src/com/bridgelabz/gamblingproblem/GamblingProblem.java
+++ src/com/bridgelabz/gamblingproblem/GamblingProblem.java
@@ -2,12 +2,42 @@ package com.bridgelabz.gamblingproblem;
 
 public class GamblingProblem 
 {
+	public static int STAKE=100;
+	public static int BET_EVERY_GAME=1;
+	
+	public static boolean makeBet()
+	{
+		boolean betWin=false;
+		int output=(int)Math.floor(Math.random()*10)%2;
+		
+		if(output==1)
+		{
+			betWin=true;
+		}
+		else if(output==0)
+		{
+			betWin=false;
+		}
+		return betWin;
+	}
 
 	public static void main(String[] args) 
 	{
-		
+		boolean betResult=false;
 		System.out.println("Welcoem to gambling problem");
-		int stake=100,betEveryGame=1;
+		
+		betResult=makeBet();
+		if(betResult==true)
+		{
+			System.out.println("Gambler won 1 dollar");
+		}
+		else
+		{
+			System.out.println("Gambler lost 1 dollar");
+
+		}
+		
+		
 		
 
 	}
